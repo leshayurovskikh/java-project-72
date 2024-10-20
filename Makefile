818af7ce52b61f23ae0d23cd1944e65460218c91
leshayu <alexei.yurovskikh@yandex.ru>
2024-10-20 11:34:14 +0500

    add MAke, chekstyle

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+	make -C app install
+
+test:
+	make -C app test
+
+report:
+	make -C app report
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    id("java")
+    checkstyle
     application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    jacoco
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 application { mainClass.set("hexlet.code.App") }
 group = "hexlet.code"
@@ -11,21 +18,36 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/io.javalin/javalin
-    testImplementation("io.javalin:javalin:6.3.0")
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
-    testImplementation("org.slf4j:slf4j-simple:2.0.13")
-    implementation("io.javalin:javalin:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("gg.jte:jte:3.1.9")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.mockito:mockito-core:5.13.0")
+    implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin-rendering:6.3.0")
+    implementation("io.javalin:javalin-bundle:6.3.0")
+    implementation("io.javalin:javalin-testtools:6.3.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    // https://mvnrepository.com/artifact/gg.jte/jte
+    implementation("gg.jte:jte:3.1.12")
+    // https://mvnrepository.com/artifact/com.zaxxer/HikariCP
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.h2database:h2:2.2.220")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("com.konghq:unirest-java:3.14.5")
+    implementation("com.konghq:unirest-objectmapper-jackson:4.2.9")
+    implementation("org.jsoup:jsoup:1.18.1")
 
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
